af2513e1f61b811f1574fd6c4bf499d4bd0e1e7d
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-10-09 13:46:33 -0700

    foreach

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -6,7 +6,7 @@ fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
 //fraudulentOrderIDs[3] = "D000"; */
 
-string[] fraudulentOrderIDs = { "A123", "B456", "C789"};
+/* string[] fraudulentOrderIDs = { "A123", "B456", "C789"};
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
@@ -15,4 +15,24 @@ Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 fraudulentOrderIDs[0] = "F000";
 Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process. ");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process. "); */
+
+// Looping using foreach
+
+// string[] names = {"Rowena", "Robin", "Bao" };
+
+// foreach(string name in names){
+//     Console.WriteLine(name);
+// }
+
+int[] inventory = {200, 450, 700, 175, 250};
+int sum = 0;
+int bin = 0;
+foreach(int items in inventory){
+    bin++;
+    sum += items;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
