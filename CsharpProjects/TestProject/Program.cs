ddc54d4d9e03c3829c394ba9091f79c16753daac
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-10-11 10:54:41 -0700

    code 1 project

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -255,12 +255,47 @@ Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to p
 
 
 
+// int numericValue = 0;
+// bool validNumber = false;
+// string? readResult;
+// Console.WriteLine("Enter a string containing at least three characters:");
+// readResult = Console.ReadLine();
+
+// validNumber = int.TryParse(readResult, out numericValue);
+// Console.WriteLine($" number is : {numericValue}");
+
+
+
+//////COde project 1 - write code that validates integer input
+string? readResult;
+Console.WriteLine("Please enter a number between 5 and 10.");
 int numericValue = 0;
 bool validNumber = false;
-string? readResult;
-Console.WriteLine("Enter a string containing at least three characters:");
-readResult = Console.ReadLine();
 
-validNumber = int.TryParse(readResult, out numericValue);
-Console.WriteLine($" number is : {numericValue}");
+do
+{
+    readResult = Console.ReadLine();
+    validNumber = int.TryParse(readResult, out numericValue);
+
+    if (validNumber == true)
+    {
+        if (numericValue >= 5 && numericValue <= 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"Your input value {numericValue} has been Accespted");
+            break;
+        }
+        else
+        {
+            validNumber = false;
+            Console.WriteLine($" you entered {numericValue}, please enter value between 5 and 10");
+        }
+    }
+    else
+    {
+        
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+
+} while (validNumber == false);
 
