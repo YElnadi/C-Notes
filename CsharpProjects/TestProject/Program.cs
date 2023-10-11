c6c24c69badff027bae5d1520c24b779850c1971
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-10-11 11:30:06 -0700

    string input

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -267,35 +267,67 @@ Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to p
 
 
 //////COde project 1 - write code that validates integer input
-string? readResult;
-Console.WriteLine("Please enter a number between 5 and 10.");
-int numericValue = 0;
-bool validNumber = false;
+// string? readResult;
+// Console.WriteLine("Please enter a number between 5 and 10.");
+// int numericValue = 0;
+// bool validNumber = false;
 
+// do
+// {
+//     readResult = Console.ReadLine();
+//     validNumber = int.TryParse(readResult, out numericValue);
+
+//     if (validNumber == true)
+//     {
+//         if (numericValue >= 5 && numericValue <= 10)
+//         {
+//             validNumber = false;
+//             Console.WriteLine($"Your input value {numericValue} has been Accespted");
+//             break;
+//         }
+//         else
+//         {
+//             validNumber = false;
+//             Console.WriteLine($" you entered {numericValue}, please enter value between 5 and 10");
+//         }
+//     }
+//     else
+//     {
+
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//     }
+
+// } while (validNumber == false);
+
+
+
+/////////Code project 2 - write code that validates string input
+
+// string role = "Administrator|Manager|User";
+string role = "";
+string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
 do
 {
     readResult = Console.ReadLine();
-    validNumber = int.TryParse(readResult, out numericValue);
-
-    if (validNumber == true)
+    if (readResult != null)
     {
-        if (numericValue >= 5 && numericValue <= 10)
-        {
-            validNumber = false;
-            Console.WriteLine($"Your input value {numericValue} has been Accespted");
-            break;
-        }
-        else
-        {
-            validNumber = false;
-            Console.WriteLine($" you entered {numericValue}, please enter value between 5 and 10");
-        }
+        role = readResult.Trim();
     }
+
+    if (role.Trim().ToLower() == "administrator" || role.Trim().ToLower() == "manager" || role.Trim().ToLower() == "user")
+    {
+       validEntry = true;
+    }
+
     else
     {
-        
-        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+
+        Console.WriteLine($"The role name that you entered, {role} is not valid. Enter your role name (Administrator, Manager, or User)");
+       
     }
 
-} while (validNumber == false);
 
+} while (validEntry == false);
+Console.WriteLine($"Your input value ({role}) has been accepted.");
